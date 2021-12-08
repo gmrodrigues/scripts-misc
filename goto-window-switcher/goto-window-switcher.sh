52e1091809fe94e811d2aa85d8ea1a4cc4fa265a
Glauber <glauber.rodrigues@allin.com.br>
2021-12-08 19:07:13 -0300

    added goto-window-switcher

--- goto-window-switcher/goto-window-switcher.sh
+++ goto-window-switcher/goto-window-switcher.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # command to swith to window by process name
+# usefull when you dont know the title of the window to use wmctrl -a alone
 # for example: ./goto-window-switcher.sh youtube-music
 set -x
 CMD_SEARCH_NAME=$*
@@ -8,4 +9,3 @@ CMD_PID=$(ps aux | grep  "$CMD_SEARCH_NAME" | awk '{print $2}' | xargs echo | se
 #sudo apt  install wmctrl
 WINDOW_REF=$(wmctrl -lp | grep -E "($CMD_PID)" | awk '{print $5" "$6" "$7" "$8" "$9" "$10" "}')
  wmctrl -a  $WINDOW_REF
- 
