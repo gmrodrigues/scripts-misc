b9f7d963195f5269408a91e4b4f596f4ca26d756
Glauber <glauber.rodrigues@allin.com.br>
2022-04-04 22:25:49 -0300

    added 11 hamonic

--- schumann-wave-generator/schumman-wave.sh
+++ schumann-wave-generator/schumman-wave.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-ffplay -f lavfi -i "sine=frequency=7.83" -nodisp
-
+ffplay -f lavfi -i "sine=frequency=7.83" -nodisp &
+#ffplay -f lavfi -i "sine=frequency=15.66" -nodisp &
+ffplay -f lavfi -i "sine=frequency=63.61" -nodisp &
+echo "press anykey to exit"
+read keypress
+killall ffplay
