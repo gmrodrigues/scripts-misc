3db0dc8c0fbb6b72a5bd68eadfc32a135cad4620
Glauber <glauber.rodrigues@allin.com.br>
2022-12-07 12:41:54 -0300

    cassandra copy to scripts

---
+++ cassandra-copy-to/cassandra-copy-to.pl
@@ -0,0 +1,35 @@
+#!/bin/env perl
+
+use strict;
+use warnings;
+
+my $FROM_SERVER='10.33.245.222'; # change here
+my $TO_SERVER='177.153.231.114'; # change here
+my $CSV_PATH='/tmp'; # change here
+
+my $KEYSPACE='btg360'; # change here
+my $CQLCMD='cqlsh-astra/bin/cqlsh --connect-timeout=15'; # change here
+
+my $GREP_TABLES_CMD="grep '_11255'"; # change here
+
+my $help = 1;
+
+while (<>) {
+    $_ =~ s/^\s+|\s+$//g;
+    my $table_name = $_;
+    last if $table_name eq '';
+    $help = 0;
+
+    my $filename = "$CSV_PATH/$table_name.csv";
+    my $copy_to_cmd = "COPY $KEYSPACE.$table_name TO   $filename;";
+    my $copy_from_cmd = "COPY $KEYSPACE.$table_name FROM $filename;";
+    print "\n#table table_name from $FROM_SERVER to $TO_SERVER\n";
+    print "\n$CQLCMD $FROM_SERVER -e \"$copy_from_cmd\"\n";
+    print "\n$CQLCMD $TO_SERVER -e \"$copy_to_cmd\"\n";
+}
+
+
+if ($help) {
+    print "# list tables\n";
+    print "$CQLCMD $FROM_SERVER -e \"paging off; SELECT table_name FROM system_schema.tables WHERE  keyspace_name = '$KEYSPACE';\" | $GREP_TABLES_CMD | perl $0\n";
+}
